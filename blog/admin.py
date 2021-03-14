1bdeefd26ccc912a57ce96e8e28eb53c17aa17d7
fercho23 <fernandoarielmateos@gmail.com>
2021-03-14 10:35:05 -0300

    Blog Fixture

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
 from blog.models.User import User
+from blog.models.Community import Community
 from blog.models.Language import Language
 from blog.models.Tag import Tag
 from blog.models.Post import Post
 
 
 admin.site.register(User)
+admin.site.register(Community)
 admin.site.register(Language)
 admin.site.register(Tag)
 admin.site.register(Post)
