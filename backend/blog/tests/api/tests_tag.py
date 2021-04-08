28d9e25c155f3c5e8c581137d1fe2e11e1db152c
fercho23 <fernandoarielmateos@gmail.com>
2021-04-08 12:08:47 -0300

    Tag Test Refactoring

--- backend/blog/tests/api/tests_tag.py
+++ backend/blog/tests/api/tests_tag.py
@@ -1,17 +1,12 @@
 
 import json
 
-from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from rest_framework import status
 from rest_framework.test import APIClient
 
 from blog.models import Tag
-
-from blog.serializers.TagSerializer import TagModelSerializer
-
-User = get_user_model()
 
 
 class TagTests(TestCase):
@@ -28,21 +23,19 @@ class TagTests(TestCase):
         response = client.get('/api/tag/list/')
         result = json.loads(response.content)
 
-        objects_query = Tag.objects
-        objects_count = objects_query.count()
-        objects = objects_query.all()[:10]
-        serialization = TagModelSerializer(objects, many=True).data
+        objects_count = 23
+        first_uuid = '4a408eff-7f22-44df-aa48-d3947321d6e8'
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(result.get('count', None), objects_count)
-        self.assertEqual(result.get('results', None), serialization)
+        self.assertEqual(result.get('results', None)[0]['uuid'], first_uuid)
         self.assertIn('next', result)
         self.assertIn('previous', result)
 
     def test_tag_list_filtered_by_name(self):
         """ test_tag_list_filtered_by_name - Tag List filtered by name """
 
-        first_obj = Tag.objects.first()
+        first_obj = Tag.objects.get(pk=2)
 
         data = {
             'name': first_obj.name[:-1],
@@ -52,15 +45,12 @@ class TagTests(TestCase):
         response = client.get('/api/tag/list/', data)
         result = json.loads(response.content)
 
-        objects_query = Tag.objects
-        objects_query = objects_query.filter(name__icontains=data['name'])
-        objects_count = objects_query.count()
-        objects = objects_query.all()[:10]
-        serialization = TagModelSerializer(objects, many=True).data
+        objects_count = 1
+        first_uuid = '3a613c48-dcb0-4b76-8362-af3cfce85141'
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(result.get('count', None), objects_count)
-        self.assertEqual(result.get('results', None), serialization)
+        self.assertEqual(result.get('results', None)[0]['uuid'], first_uuid)
         self.assertIn('next', result)
         self.assertIn('previous', result)
 
@@ -75,14 +65,11 @@ class TagTests(TestCase):
         response = client.get('/api/tag/list/', data)
         result = json.loads(response.content)
 
-        objects_query = Tag.objects
-        objects_query = objects_query.order_by(data['ordering'])
-        objects_count = objects_query.count()
-        objects = objects_query.all()[:10]
-        serialization = TagModelSerializer(objects, many=True).data
+        objects_count = 23
+        first_uuid = '40bed8ac-9b0b-4ad3-bc18-afa4b793cb5f'
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(result.get('count', None), objects_count)
-        self.assertEqual(result.get('results', None), serialization)
+        self.assertEqual(result.get('results', None)[0]['uuid'], first_uuid)
         self.assertIn('next', result)
         self.assertIn('previous', result)
