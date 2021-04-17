68ddd150713971bebb5367974fd8bd286a48507f
fercho23 <fernandoarielmateos@gmail.com>
2021-04-16 21:46:53 -0300

    Frontend - Post List

--- frontend/src/components/Posts/PostList.vue
+++ frontend/src/components/Posts/PostList.vue
@@ -1,37 +1,46 @@
 <template>
-    <div class="row">
-        <div class="col-12">
-            <h3>Post List</h3>
-            <div class="row">
-                <div class="col-6" v-for="(post, index) in posts" :key="index">
-                    <router-link :to="{
-                        name: 'post-details',
-                        params: { post: post, slug: post.slug }
-                      }">
-                      {{post.title}}
-                    </router-link>
-                </div>
-            </div>
+  <div class="row">
+    <div class="col-12">
+      <router-link
+        v-if="accessToken!=null"
+        class="btn btn-primary float-right"
+        :to="{
+          name: 'post-create'
+        }">
+        Create Post
+      </router-link>
+      <h3>Post List</h3>
+
+      <div class="row">
+        <div class="col-6" v-for="(post, index) in posts" :key="index">
+          <router-link :to="{
+              name: 'post-details',
+              params: { post: post, slug: post.slug }
+            }">
+            {{post.title}}
+          </router-link>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-  import { getAPI } from '../../api/axios-base'
+  import { mapState } from 'vuex';
+  import { getAPI } from '../../api/axios-base';
   import { URL_API_POST_LIST } from '../../constants.js';
 
   export default {
     name: "post-list",
+    computed: mapState(['accessToken']),
     data() {
       return {
         posts: []
       };
     },
     methods: {
-      /* eslint-disable no-console */
       retrievePostList() {
-        getAPI
-          .get(URL_API_POST_LIST)
+        getAPI.get(URL_API_POST_LIST)
           .then(response => {
             this.posts = response.data.results;
           })
@@ -39,7 +48,6 @@
             console.log(e);
           });
       }
-      /* eslint-enable no-console */
     },
     mounted() {
       this.retrievePostList();
